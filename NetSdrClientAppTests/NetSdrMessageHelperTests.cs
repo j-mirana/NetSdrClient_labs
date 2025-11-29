f601bb3b691522761dbad981019f9b2cce2ec5e8
j-mirana <lanafasta@gmail.com>
2025-11-29 04:53:31 +0200

    refactor: fix NUnit asserts to satisfy SonarCloud (Assert.Multiple, Has.Length)

--- NetSdrClientAppTests/NetSdrMessageHelperTests.cs
+++ NetSdrClientAppTests/NetSdrMessageHelperTests.cs
@@ -1,95 +1,101 @@
 using NetSdrClientApp.Messages;
 using NUnit.Framework;
-
+using System;
 
 namespace NetSdrClientAppTests
 {
     public class NetSdrMessageHelperTests
     {
-
         [Test]
         public void GetControlItemMessageTest()
         {
-            //Arrange
+            // Arrange
             var type = NetSdrMessageHelper.MsgTypes.Ack;
             var code = NetSdrMessageHelper.ControlItemCodes.ReceiverState;
             int parametersLength = 7500;
 
-            //Act
+            // Act
             byte[] msg = NetSdrMessageHelper.GetControlItemMessage(type, code, new byte[parametersLength]);
 
-            var headerBytes = msg.Take(2);
-            var codeBytes = msg.Skip(2).Take(2);
-            var parametersBytes = msg.Skip(4);
+            var headerBytes = msg.Take(2).ToArray();
+            var codeBytes = msg.Skip(2).Take(2).ToArray();
+            var parametersBytes = msg.Skip(4).ToArray();
 
-            var num = BitConverter.ToUInt16(headerBytes.ToArray());
+            var num = BitConverter.ToUInt16(headerBytes);
             var actualType = (NetSdrMessageHelper.MsgTypes)(num >> 13);
             var actualLength = num - ((int)actualType << 13);
-            var actualCode = BitConverter.ToInt16(codeBytes.ToArray());
+            var actualCode = BitConverter.ToInt16(codeBytes);
 
-            //Assert
-            Assert.That(headerBytes.Count(), Is.EqualTo(2));
-            Assert.That(msg.Length, Is.EqualTo(actualLength));
-            Assert.That(type, Is.EqualTo(actualType));
-
-            Assert.That(actualCode, Is.EqualTo((short)code));
-
-            Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+            // Assert (Sonar-friendly)
+            Assert.Multiple(() =>
+            {
+                Assert.That(headerBytes, Has.Length.EqualTo(2));
+                Assert.That(msg, Has.Length.EqualTo(actualLength));
+                Assert.That(actualType, Is.EqualTo(type));
+                Assert.That((short)code, Is.EqualTo(actualCode));
+                Assert.That(parametersBytes, Has.Length.EqualTo(parametersLength));
+            });
         }
 
         [Test]
         public void GetDataItemMessageTest()
         {
-            //Arrange
+            // Arrange
             var type = NetSdrMessageHelper.MsgTypes.DataItem2;
             int parametersLength = 7500;
 
-            //Act
+            // Act
             byte[] msg = NetSdrMessageHelper.GetDataItemMessage(type, new byte[parametersLength]);
 
-            var headerBytes = msg.Take(2);
-            var parametersBytes = msg.Skip(2);
+            var headerBytes = msg.Take(2).ToArray();
+            var parametersBytes = msg.Skip(2).ToArray();
 
-            var num = BitConverter.ToUInt16(headerBytes.ToArray());
+            var num = BitConverter.ToUInt16(headerBytes);
             var actualType = (NetSdrMessageHelper.MsgTypes)(num >> 13);
             var actualLength = num - ((int)actualType << 13);
 
-            //Assert
-            Assert.That(headerBytes.Count(), Is.EqualTo(2));
-            Assert.That(msg.Length, Is.EqualTo(actualLength));
-            Assert.That(type, Is.EqualTo(actualType));
-
-            Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+            // Assert (Sonar-friendly)
+            Assert.Multiple(() =>
+            {
+                Assert.That(headerBytes, Has.Length.EqualTo(2));
+                Assert.That(msg, Has.Length.EqualTo(actualLength));
+                Assert.That(actualType, Is.EqualTo(type));
+                Assert.That(parametersBytes, Has.Length.EqualTo(parametersLength));
+            });
         }
 
         // COVERAGE: Test for ArgumentOutOfRangeException in GetSamples
         [Test]
         public void GetSamples_ThrowsException_WhenSampleSizeIsTooLarge()
         {
-            // Arrange: 4 bytes (32 bit) is max size. 40 bit is 5 bytes, which should fail.
-            ushort sampleSize = 40;
+            ushort sampleSize = 40; // too big
             byte[] body = { 0x01, 0x02, 0x03, 0x04, 0x05 };
 
-            // Act & Assert
-            Assert.Throws<ArgumentOutOfRangeException>(() => NetSdrMessageHelper.GetSamples(sampleSize, body).ToArray());
+            Assert.That(
+                () => NetSdrMessageHelper.GetSamples(sampleSize, body).ToArray(),
+                Throws.TypeOf<ArgumentOutOfRangeException>()
+            );
         }
 
         // COVERAGE: Test for successful 16-bit sample retrieval
         [Test]
         public void GetSamples_ReturnsCorrectSamples_For16Bit()
         {
-            // Arrange: 16 bit samples (2 bytes each). Total 3 samples.
+            // Arrange: 3 samples (16-bit = 2 bytes each)
             byte[] body = { 0x01, 0x00, 0x02, 0x00, 0x00, 0x00 };
             ushort sampleSize = 16;
 
             // Act
             var samples = NetSdrMessageHelper.GetSamples(sampleSize, body).ToArray();
 
-            // Assert
-            Assert.That(samples.Length, Is.EqualTo(3));
-            Assert.That(samples[0], Is.EqualTo(1));
-            Assert.That(samples[1], Is.EqualTo(2));
-            Assert.That(samples[2], Is.EqualTo(0));
+            // Assert (Sonar-friendly)
+            Assert.Multiple(() =>
+            {
+                Assert.That(samples, Has.Length.EqualTo(3));
+                Assert.That(samples[0], Is.EqualTo(1));
+                Assert.That(samples[1], Is.EqualTo(2));
+                Assert.That(samples[2], Is.EqualTo(0));
+            });
         }
     }
 }
